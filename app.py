8116c69d96ea179a502aba726a19aae05cc429b7
Manas Nagelia <84251015+Manas-Nagelia@users.noreply.github.com>
2024-03-28 01:00:49 -0400

    Fixed critical bug with buy limit orders
    Previously, buy limits that were priced over the best sell price were not respecting the buyer's limit, and therefore operating as a market order.

--- app.py
+++ app.py
@@ -19,11 +19,11 @@ class Security:
                 "ticker": self.ticker,
                 "side": side,
                 "price": price,
-                "size": quantity
+                "size": quantity,
+                "type": "limit"
             })
 
         self.match_orders()
-
     def create_market_order(self, side, quantity):
         buy_orders, sell_orders = self.__update_bid_ask()
 
@@ -32,14 +32,16 @@ class Security:
                 "ticker": self.ticker,
                 "side": side,
                 "price": sell_orders[0]["price"],
-                "size": quantity
+                "size": quantity,
+                "type": "market"
             })
         else:
             self.order_book.append({
                 "ticker": self.ticker,
                 "side": side,
                 "price": buy_orders[0]["price"],
-                "size": quantity
+                "size": quantity,
+                "type": "market"
             })
 
         self.match_orders()
@@ -73,32 +75,58 @@ class Security:
             buy_order = buy_orders[buy_index]
             sell_order = sell_orders[sell_index]
 
+            if buy_order is None or sell_order is None:
+                break
+
             # If we have a buy and sell order, and the buy order is greater than or equal than the sell order
-            if buy_order is not None and sell_order is not None and buy_order["price"] >= sell_order["price"]:
+            if buy_order["type"] == "limit" and buy_order["price"] >= sell_order["price"]:
                 trade_price = sell_order["price"]
                 trade_quantity = min(buy_order["size"], sell_order["size"])
-                self.trade_history.append({trade_price, trade_quantity})
+                self.trade_history.append({"price": trade_price, "quantity": trade_quantity})
                 buy_order["size"] -= trade_quantity
                 sell_order["size"] -= trade_quantity
 
-                # Useless because going down the bidders will never result in an execution to the seller
-                # if buy_order.quantity == 0:
-                #     buy_index += 1
-
-                # okay in edge case if buyer is offering to buy higher than the current seller
-                if sell_order["size"] == 0:
+                # If this buy order is overpaying, and they still have shares left, there is a possibility they can
+                # get the rest of their shares filled, so we'll move up the sellers list
+                if sell_order["size"] == 0 and buy_order["size"] > 0 and buy_order["price"] > sell_order["price"]:
+                    # Note that this may not be filled if it does not meet the limit criteria of the buyer
                     sell_index += 1
+                else:
+                    break
+            elif buy_order["type"] == "market":
+                trade_price = sell_order["price"]
+                trade_quantity = min(buy_order["size"], sell_order["size"])
+                self.trade_history.append({"price": trade_price, "quantity": trade_quantity})
+                buy_order["size"] -= trade_quantity
+                sell_order["size"] -= trade_quantity
+
+                if sell_order["size"] == 0 and buy_order["size"] > 0:
+                    sell_index += 1
+                else:
+                    break
             else:
+                # If the bids and asks cannot match up, then the market cannot execute an order
                 break
 
 
+
 my_stock = Security("AAPL")
-print(my_stock.order_book)
+# print(my_stock.order_book)
 my_stock.create_limit_order("sell", 100.0, 5)
-print(my_stock.order_book)
-my_stock.create_limit_order("sell", 99.94, 15)
-print(my_stock.order_book)
-my_stock.create_limit_order("sell", 99.99, 2)
-print(my_stock.order_book)
-my_stock.create_market_order("buy", 20)
-print(my_stock.order_book)
+# print(my_stock.order_book)
+my_stock.create_limit_order("sell", 101.0, 5)
+# print(my_stock.order_book)
+my_stock.create_limit_order("sell", 102.0, 5)
+# print(my_stock.order_book)
+my_stock.create_limit_order("sell", 103.0, 5)
+# print(my_stock.order_book)
+my_stock.create_limit_order("sell", 104.0, 5)
+# print(my_stock.order_book)
+my_stock.create_limit_order("sell", 105.0, 5)
+# print(my_stock.order_book)
+# my_stock.create_limit_order("sell", 99.94, 10)
+# print(my_stock.order_book)
+# my_stock.create_limit_order("sell", 99.99, 2)
+# print(my_stock.order_book)
+my_stock.create_market_order("buy", 50)
+print(my_stock.trade_history)
