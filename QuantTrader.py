97027fccc626b88e6f9761821204938826a85ffd
Manas Nagelia <84251015+Manas-Nagelia@users.noreply.github.com>
2024-07-30 21:35:21 -0400

    Tested out QuantTrader

--- QuantTrader.py
+++ QuantTrader.py
@@ -1,22 +1,22 @@
-from Trader import Trader
-import torch
-import torch.nn as nn
-import pandas as pd
-import yfinance as yf
-import numpy as np
 from QuantPredictor import QuantPredictor
+from Trader import Trader
+
 
 # LSTM Will give price prediciton on the next day
 # Based on that hey buy or sell
 # Parameters -- reinforcing, does either adjust parameter of model or adjust trust in the model
 
 
-class quantTrader(Trader):
+class QuantTrader(Trader):
     def __init__(self, balance):
         super().__init__(True, balance)
 
-    def getStockRec(self, stock):
+    def get_stock_recc(self, stock):
         predictor = QuantPredictor(lookback=7, hidden_size=16,
                                    num_stacked_layers=2, batch_size=16, learning_rate=0.003, num_epochs=30)
         result_df = predictor.predict_stock(str(stock))
         return result_df
+
+
+my_trader = QuantTrader(500)
+my_trader.get_stock_recc("TSLA")
