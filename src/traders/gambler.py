21766e031234f8324fbf683b8697e0228d16f1f8
namanNagelia <nagelia.naman@gmail.com>
2024-09-16 11:00:51 -0400

    Gambler done

--- src/traders/gambler.py
+++ src/traders/gambler.py
@@ -19,4 +19,15 @@ class Gambler:
 
     def choose_stock(self):
         stocks = self.get_stocks()
-        # Make this choose a stock with the hihgest beta and volatility
+        stocks = stocks.data
+        # CHooses tock if beta over 1.3 which is considered high
+        high_beta_stocks = [
+            stock for stock in stocks if stock.get('beta') is not None and stock.get('beta', 0) > 1.3
+        ]
+
+        high_beta_stocks.sort(key=lambda x: x.get('volume', 0), reverse=True)
+
+        high_beta_stocks.sort(key=lambda x: x.get('beta', 0), reverse=True)
+        chosen_stock = random.choice(high_beta_stocks)
+
+        return chosen_stock
--- tests.py
+++ tests.py
@@ -2,4 +2,4 @@ from src.traders.technicalTrader import TechnicalTrader
 from src.traders.gambler import Gambler
 
 testGambler = Gambler()
-print(testGambler.get_stocks())
+print(testGambler.choose_stock())
